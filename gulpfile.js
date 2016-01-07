d20a574f3ee0cdeef5a532bd788c96ac2229a53c
haishanh <haishanhan@gmail.com>
2016-01-07 18:36:00 +0800

    Update style for footer

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ var gulp = require('gulp'),
     rename= require('gulp-rename');
 
 gulp.task('less', function () {
-  gulp.src('themes/default/resources/css/_main.less')
+  gulp.src('themes/default/assets/css/_main.less')
     .pipe(less())
     .pipe(rename('main.css'))
-    .pipe(gulp.dest('themes/default/resources/css/'));
+    .pipe(gulp.dest('themes/default/assets/css/'));
 });
 
 gulp.task('default', ['less']);

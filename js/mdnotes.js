e731d372601e43da5592a5d985109b0f6a52b594
haishanh <haishanhan@gmail.com>
2016-02-19 14:17:45 +0800

    site update 16/02/19 14:17:45

---
+++ js/mdnotes.js
@@ -0,0 +1,51 @@
+function addAnchors() {
+  var all_headings = ['h1, ', 'h2, ', 'h3, ', 'h4, ', 'h5, ', 'h6, ', 'h7'];
+  var article_headings = '';
+  for (var i=0; i<all_headings.length; i++) {
+    article_headings += 'article > ' + all_headings[i];
+  }
+  for (var a = document.querySelectorAll(article_headings), b = 0, c = a.length; c > b; b++) {
+    var d = a[b],
+      e = document.createElement("a");
+    e.href = "#" + d.id, e.textContent = "#", e.className = "header-anchor", d.insertBefore(e, d.firstChild);
+    d.onmouseover = function () { this.firstElementChild.style.display = 'inline'; };
+    d.onmouseout  = function () { this.firstElementChild.style.display = 'none'; };
+  }
+}
+
+function stickToc() {
+  var toc = $("#toc");
+  var window_top = $(window).scrollTop();
+  var left_top = $("#left").offset().top;
+  if (window_top > left_top) {
+    toc.addClass("fixed");
+  } else {
+    toc.removeClass("fixed");
+  }
+}
+
+(function () {
+  var dropdownElements = document.getElementsByClassName('dropdown-select');
+  for (var i = 0; i < dropdownElements.length; i++) {
+    dropdownElements[i].onchange = function () {
+      window.location.href = this.value;
+    };
+  }
+  addAnchors();
+  //$(window).scroll(stickToc);
+
+  // scrollspy
+  gumshoe.init({
+    selector: '.toc li a',
+    selectorHeader: '.toc',
+    offset: 0,
+    activeClass: 'active',
+    callback: function (nav) {}
+  });
+})();
+
+
+$(function () {
+  $(window).scroll(stickToc);
+  stickToc();
+});
